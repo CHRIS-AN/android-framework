adec7750579cff621f13972b5b052031b96a765b
JUNGMIN AN <71064490+CHRIS-AN@users.noreply.github.com>
2022-11-30 16:56:58 +0900

    feat : <data> 태그를 이용하여 데이터 클래스를 뷰에 직접 사용할 수 있도록 함.

--- app/src/main/java/com/chrisan/framwork/databinding/DatabindingActivity.kt
+++ app/src/main/java/com/chrisan/framwork/databinding/DatabindingActivity.kt
@@ -10,6 +10,7 @@ import com.chrisan.framwork.R
 
 class DatabindingActivity : AppCompatActivity() {
     private lateinit var binding: ActivityDatabindingBinding
+    private val myName: MyName = MyName("Chris An")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,17 +19,21 @@ class DatabindingActivity : AppCompatActivity() {
         binding.doneButton.setOnClickListener {
             addNickname(it)
         }
+
+        binding.myName = myName
     }
 
     /**
      * Click handler for the Done button.
      */
     private fun addNickname(view: View) {
-        binding.nicknameText.text = binding.nicknameEdit.text
-        binding.nicknameEdit.visibility = View.GONE
-        binding.doneButton.visibility = View.GONE
-        binding.nicknameText.visibility = View.VISIBLE
-
+        binding.apply {
+            myName?.nickname = binding.nicknameEdit.text.toString()
+            invalidateAll()
+            nicknameEdit.visibility = View.GONE
+            doneButton.visibility = View.GONE
+            nicknameText.visibility = View.VISIBLE
+        }
 
         // Hide the keyboard.
         val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
---
+++ app/src/main/java/com/chrisan/framwork/databinding/MyName.kt
@@ -0,0 +1,3 @@
+package com.chrisan.framwork.databinding
+
+data class MyName(var name: String = "", var nickname: String = "")
