c9f12f4c69a1d0a9e5c0e70b73916fa7ab8767df
JUNGMIN AN <71064490+CHRIS-AN@users.noreply.github.com>
2022-12-01 16:55:30 +0900

    feat : room 데이터 베이스 세팅

--- room/app/src/main/java/com/example/android/trackmysleepquality/database/SleepDatabase.kt
+++ room/app/src/main/java/com/example/android/trackmysleepquality/database/SleepDatabase.kt
@@ -1,17 +1,47 @@
-/*
- * Copyright 2019, The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.example.android.trackmysleepquality.database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+/**
+    스키마 버전 기록 백업을 유지하지 않으려면 로 설정 exportSchema 를 false,
+    스키마 버전은 1부터 스키마를 변경할 때마다 1씩 up
+ */
+@Database(entities = [SleepNight::class], version = 1, exportSchema = false)
+abstract class SleepDatabase : RoomDatabase() {
+
+    abstract val sleepDatabaseDao: SleepDatabaseDao
+
+    companion object {
+
+        /**
+            @Volatile 은 휘발성 변수의 값이 캐시되지 않으며 모든 쓰기 및 읽기는 주 메모리에서 수행됩니다.
+            INSTANCE 값이 항상 최신 상태이고 모든 실행 스레드에 대해 동일한지 확인하는 데 도움이 됩니다.
+            즉, 한 스레드에서 변경한 내용을 INSTANCE다른 모든 스레드에서 즉시 볼 수 있으며,
+            두 스레드가 각각 캐시에서 동일한 엔터티를 업데이트하여 문제를 일으키는 상황이 발생하지 않습니다.
+         */
+        @Volatile
+        private var INSTANCE: SleepDatabase? = null
+
+        fun getInstance(context: Context): SleepDatabase {
+            /** synchronized 는 한 번에 하나의 실행 스레드만 이 코드 블록에 들어갈 수 있으므로 데이터베이스가 한 번만 초기화됩니다. */
+            synchronized(this) {
+                var instance = INSTANCE
+
+                if (instance == null) { /** 이렇게 하면 계산 비용이 많이 드는 데이터베이스에 대한 연결을 반복적으로 여는 것을 방지할 수 있습니다 */
+                    instance = Room.databaseBuilder(
+                        context.applicationContext,
+                        SleepDatabase::class.java,
+                        "sleep_history_database"
+                    )
+                        .fallbackToDestructiveMigration()
+                        .build()
+                    INSTANCE = instance
+                }
+                return instance
+            }
+        }
+    }
+}
--- room/app/src/main/java/com/example/android/trackmysleepquality/database/SleepNight.kt
+++ room/app/src/main/java/com/example/android/trackmysleepquality/database/SleepNight.kt
@@ -1,18 +1,25 @@
-/*
- * Copyright 2019, The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.example.android.trackmysleepquality.database
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+
+/**
+ * 보통, 테이블 이름은 클래스 이름과 동일하지만,
+ * 아래에 tableName 을 지정해서 테이블 명을 정할 수 있다. 이 방법을 추천한다.
+ */
+@Entity(tableName = "daily_sleep_quality_table")
+data class SleepNight(
+    @PrimaryKey(autoGenerate = true)
+    var nightId: Long = 0L,
+
+    @ColumnInfo(name = "start_time_milli")
+    val startTimeMilli: Long = System.currentTimeMillis(),
+
+    @ColumnInfo(name = "end_time_milli")
+    var endTimeMilli: Long = startTimeMilli,
+
+    @ColumnInfo(name = "quality_rating")
+    var sleepQuality: Int = -1
+)
