6c675697cc6886c3833ad8adb4604d4b874b3bde
JUNGMIN AN <71064490+CHRIS-AN@users.noreply.github.com>
2022-12-20 16:52:14 +0900

    refactor : list viewholder 내부 class 로 선언 한 뒤, bind

--- recyclerview/app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
+++ recyclerview/app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
@@ -3,14 +3,17 @@ package com.example.android.trackmysleepquality.sleeptracker
 import android.annotation.SuppressLint
 import android.graphics.Color
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.android.trackmysleepquality.R
-import com.example.android.trackmysleepquality.TextItemViewHolder
+import com.example.android.trackmysleepquality.convertDurationToFormatted
+import com.example.android.trackmysleepquality.convertNumericQualityToString
 import com.example.android.trackmysleepquality.database.SleepNight
 
-class SleepNightAdapter: RecyclerView.Adapter<TextItemViewHolder>() {
+class SleepNightAdapter: RecyclerView.Adapter<SleepNightAdapter.ViewHolder>() {
 
     var data = listOf<SleepNight>()
         @SuppressLint("NotifyDataSetChanged")
@@ -19,33 +22,38 @@ class SleepNightAdapter: RecyclerView.Adapter<TextItemViewHolder>() {
             notifyDataSetChanged()
         }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TextItemViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val layoutInflater = LayoutInflater.from(parent.context)
-        val view = layoutInflater.inflate(R.layout.text_item_view, parent, false) as TextView
+        val view = layoutInflater.inflate(R.layout.list_item_sleep_night, parent, false)
 
-        return TextItemViewHolder(view)
+        return ViewHolder(view)
     }
 
-    override fun onBindViewHolder(holder: TextItemViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val item = data[position]
-        holder.textView.text = item.sleepQuality.toString()
-
-
-        // 뷰 홀더 재사용 함에 따라서, 결국 고품질 등급을 위해 빨간색 뷰 홀더 중 하나를 재사용하게 됩니다.
-        // 그럴 때, 높은 등급이 빨간색으로 잘못 표시되는 경우가 생깁니다.
-        if (item.sleepQuality <= 1) {
-            holder.textView.setTextColor(Color.RED)
-        }
-
-        else {
-            // reset
-            // else 아래 구문을 작성해야, 두 조건이 모두 명시되어 있는 뷰 홀더를 각각 올바르게 텍스트 색상을 사용합니다.
-            holder.textView.setTextColor(Color.BLACK)
-        }
+        val res = holder.itemView.context.resources
+        holder.sleepLength.text = convertDurationToFormatted(item.startTimeMilli, item.endTimeMilli, res)
+        holder.quality.text= convertNumericQualityToString(item.sleepQuality, res)
+        holder.qualityImage.setImageResource(when (item.sleepQuality) {
+            0 -> R.drawable.ic_sleep_0
+            1 -> R.drawable.ic_sleep_1
+            2 -> R.drawable.ic_sleep_2
+            3 -> R.drawable.ic_sleep_3
+            4 -> R.drawable.ic_sleep_4
+            5 -> R.drawable.ic_sleep_5
+            else -> R.drawable.ic_sleep_active
+        })
     }
 
     // 수면 night 목록의 크기를 반환하도록 재정의
     // 어댑터가 표시할 수 있는 항목 수를 알아야 함.
     override fun getItemCount() = data.size
+
+    // 내부 클래스에서 ViewHolder 생성 후, RecyclerView.ViewHolder 로 확장
+    class ViewHolder(itemView : View): RecyclerView.ViewHolder(itemView) {
+        val sleepLength: TextView = itemView.findViewById(R.id.sleep_length)
+        val quality: TextView = itemView.findViewById(R.id.quality_string)
+        val qualityImage: ImageView = itemView.findViewById(R.id.quality_image)
+    }
 
 }
