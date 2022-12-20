475f317ec0bc766abe1513f68f69df2a867beaaf
JUNGMIN AN <71064490+CHRIS-AN@users.noreply.github.com>
2022-12-20 16:33:31 +0900

    refactor : reyclerview 재사용할 때 발생한 문제를 잡기 위한 else 키워드

--- recyclerview/app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
+++ recyclerview/app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
@@ -1,6 +1,7 @@
 package com.example.android.trackmysleepquality.sleeptracker
 
 import android.annotation.SuppressLint
+import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.TextView
@@ -29,6 +30,18 @@ class SleepNightAdapter: RecyclerView.Adapter<TextItemViewHolder>() {
         val item = data[position]
         holder.textView.text = item.sleepQuality.toString()
 
+
+        // 뷰 홀더 재사용 함에 따라서, 결국 고품질 등급을 위해 빨간색 뷰 홀더 중 하나를 재사용하게 됩니다.
+        // 그럴 때, 높은 등급이 빨간색으로 잘못 표시되는 경우가 생깁니다.
+        if (item.sleepQuality <= 1) {
+            holder.textView.setTextColor(Color.RED)
+        }
+
+        else {
+            // reset
+            // else 아래 구문을 작성해야, 두 조건이 모두 명시되어 있는 뷰 홀더를 각각 올바르게 텍스트 색상을 사용합니다.
+            holder.textView.setTextColor(Color.BLACK)
+        }
     }
 
     // 수면 night 목록의 크기를 반환하도록 재정의
